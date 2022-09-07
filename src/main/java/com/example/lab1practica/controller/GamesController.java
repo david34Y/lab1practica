3acf18522797ab06064ed6d715e589f21a449271
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-06 20:54:03 -0500

    GamesController

---
+++ src/main/java/com/example/lab1practica/controller/GamesController.java
@@ -0,0 +1,17 @@
+package com.example.lab1practica.controller;
+
+public class GamesController {
+
+    public String listaJuegos (){
+        return "";
+    };
+
+    public String editarJuegos(){
+        return "";
+    };
+
+    public String guardarJuegos(){
+        return "";
+    };
+
+}
