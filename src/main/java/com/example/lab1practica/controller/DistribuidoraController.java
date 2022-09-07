3551cc7c2728c90957d50027d490f0b79eb52643
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-06 20:51:24 -0500

    carpetas

--- src/main/java/com/example/lab1practica/controller/DistribuidoraController.java
+++ src/main/java/com/example/lab1practica/controller/DistribuidoraController.java
@@ -3,7 +3,6 @@ package com.example.lab1practica.controller;
 import com.example.lab1practica.entity.Distribuidora;
 import com.example.lab1practica.repository.DistribuidoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.Id;
 import java.util.List;
 import java.util.Optional;
 
